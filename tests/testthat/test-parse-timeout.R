f1c29372c58e39c9433e955e38c47c8eb7779d9d
Glenn McDonald <2371316+glenn-m@users.noreply.github.com>
2018-10-31 12:56:57 +0000

    Fixing unit tests for parse_timeout()

--- tests/testthat/test-parse-timeout.R
+++ tests/testthat/test-parse-timeout.R
@@ -1,21 +1,24 @@
 context("Test parse_timeout")
 
-test_that(desc = "Timeout must not be a negative number",
-          code = expect_failure(
-            expect_error(
-              parse_timeout("-5h"),
-              "Can't use negative timeout values, please use a positive number."
-            )
-          ))
+test_that(
+  desc = "Timeout must not be a negative number",
+  code =
+    expect_error(
+      parse_timeout("-5s"),
+      "Can't use negative timeout values, please use a positive number."
+    )
+)
 
-test_that(desc = "Timeout must have the unit of 's', 'h', or 'd' ",
-          code = expect_failure(
-            expect_error(
-              parse_timeout("2y"),
-              "Couldn't identify timeout value. Please use 's', 'm', or 'h'"
-            )
-          ))
+test_that(
+  desc = "Timeout must have the unit of 's', 'h', or 'd' ",
+  code =
+    expect_error(
+      parse_timeout("2y"),
+      "Timeout must be in seconds 's', minutes 'm', or hours 'h'."
+    )
+)
 
 test_that(desc = "Timeout gets converted into common unit (s)",
-          code = expect_failure(expect_error(parse_timeout("2h"),
-                                             7200)))
+          code =
+            expect_equal(parse_timeout("2h"),
+                         7200))
