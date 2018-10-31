c623d6f58c8c00440e27304a50134fdeef627ec8
tylurp <tylurp1@gmail.com>
2018-10-30 20:45:39 -0700

    add parse_timeout utility function, fixes #2

--- R/promR.R
+++ R/promR.R
@@ -10,12 +10,49 @@
 Prometheus <-
   setRefClass("Prometheus", fields = list(host = "character", port = "numeric"))
 
+#' Utility function for parsing timeout argument 'parse_timeout'
+#' @param timeout_val A timeout value assigned by user.
+parse_timeout <- function(timeout_val) {
+
+  # If timeout starts with '-' stop the function and inform the user
+  if(startsWith(timeout_val, "-"))
+    stop("Can't use negative timeout values, please use a positive number.", call. = FALSE)
+
+  # Identify the time unit
+  time_unit <- sub("[^[:alpha:]]+", "", timeout_val)
+
+  # Test if the time_unit > 1 or == 0. If it's the former, we assume they gave
+  # something like 'minutes' instead of 'm'. If it's the latter, we assume they
+  # didn't give any unit at all, e.g. '1.5'.
+  if(nchar(time_unit) > 1 | nchar(time_unit) == 0)
+    stop("Couldn't identify timeout value. Please use 's', 'm', or 'h'", call. = FALSE)
+
+  # Negation of %in% operator to test if time_unit is one of the three options
+  `%nin%` = Negate(`%in%`)
+
+  # If time_unit isn't in one of the three options, stop the function
+  if(time_unit %nin% c("s", "m", "h"))
+    stop("Timeout must be in seconds 's', minutes 'm', or hours 'h'.", call. = FALSE)
+
+  # If the time_unit is identified, assign a conversion value so all timeouts
+  # are measured in seconds.
+  if(time_unit == "s") conversion <- 1
+  if(time_unit == "m") conversion <- 60
+  if(time_unit == "h") conversion <- 3600
+
+  # Remove all non numerics, keep '.', convert to numeric and multiply by conversion value
+  timeout_seconds <- as.numeric(gsub("[^0-9.]", "", timeout_val)) * conversion
+
+  return(timeout_seconds)
+
+}
+
 #' @name Prometheus_query
 #' @title Prometheus Instant Query
 #' @description Run a instant query against the Prometheus Server
 #' @param query The PromQL query
 #' @param time Evaluation timestamp, can be a rfc3339 or unix timestamp. Optional, defaults to current Prometheus server time.
-#' @param timeout Evaluation timeout. Optional, defaults to timeout value of the Prometheus server.
+#' @param timeout Evaluation timeout (e.g. 1h, 1.5m or 15s). Optional, defaults to timeout value of the Prometheus server.
 #' @examples
 #' \dontrun{
 #' prom <- Prometheus$new(host = "https://foo.bar", port = 9090)
@@ -34,8 +71,10 @@ Prometheus$methods(
     }
 
     # If timeout is not provided it uses server default
-    if (!is.null(timeout)) {
+    if (missing(timeout)) {
       params <- c(params, timeout = timeout)
+    } else {
+      params <- c(params, timeout = parse_timeout(timeout))
     }
 
     r <-
@@ -81,8 +120,10 @@ Prometheus$methods(
     )
 
     # If timeout is not provided it uses server default
-    if (!is.null(timeout)) {
+    if (missing(timeout)) {
       params <- c(params, timeout = timeout)
+    } else {
+      params <- c(params, timeout = parse_timeout(timeout))
     }
 
     r <-
