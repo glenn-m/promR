19eadaa4b1997f4000b60e641b1852c6a32cfe2f
Konrad Zdeb <konrad.zdeb@me.com>
2019-03-30 13:48:29 +0000

    Testing
    Created more tests to check generated response
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- tests/testthat/test-prom-class.R
+++ tests/testthat/test-prom-class.R
@@ -6,19 +6,3 @@ test_that(desc = "Object of Prometheus class is created",
             class = "Prometheus"
           ))
 
-# Test whether package can pull some sample data, only run if connection
-# available and host is responsive
-if (requireNamespace(package = "curl", quietly = TRUE)) {
-  if (curl::has_internet()) {
-
-    # Source query data once to use later across tests
-    dta_tst_metrics <-
-
-    test_that(
-      desc = "Object Prometheous class is populated with data",
-      code = expect_
-
-      )
-    )
-  }
-}
