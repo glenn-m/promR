02189fb6f04b834e252d3194815d389208af0e87
Konrad Zdeb <konrad.zdeb@me.com>
2019-04-01 16:39:45 +0100

    Documentation
    Created documentation for the sample data sets.
    Guideines:
    http://r-pkgs.had.co.nz/data.html#documenting-data
    
    Issue #11
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Sample Promethous instant query results
+#'
+#' The data set contains sample query result generated via
+#'   \code{\link[promR]{Prometheus_query}} for a historical date. The data is provided
+#'   by \url{demo.robustperception.io}.
+"metrics_current"
+
+#' Sample  Promethous range query results
+#'
+#' The data set contains sample query result generated via
+#'   \code{\link[promR]{Prometheus_rangeQuery}} for a historical time frame.
+#'   The data is provided by \url{demo.robustperception.io}.
+"metrics_range"
