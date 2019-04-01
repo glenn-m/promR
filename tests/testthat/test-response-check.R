a29b3ab491e98cebd6fca8cb79744d99e2c8858b
Glenn McDonald <2371316+glenn-m@users.noreply.github.com>
2019-04-01 19:48:20 +0100

    Adding in test for json assertion

--- tests/testthat/test-response-check.R
+++ tests/testthat/test-response-check.R
@@ -1,25 +1,45 @@
 context("Test Prometheus Response Check")
 
-test_that(
-  desc = "Error is raised if 400 received from server.",
-  code = {
-    response <- list(status_code = 400)
-    expect_error(response_check(response))
-})
+test_that(desc = "Error is raised if 400 received from server.",
+          code = {
+            response <- list(status_code = 400)
+            expect_error(response_check(response))
+          })
 
-test_that(
-  desc = "Error is raised if 422 received from server.",
-  code = {
-    response <- list(status_code = 422)
-    expect_error(response_check(response))
-})
+test_that(desc = "Error is raised if 422 received from server.",
+          code = {
+            response <- list(status_code = 422)
+            expect_error(response_check(response))
+          })
 
-test_that(
-  desc = "Error is raised if 503 received from server.",
-  code = {
-    response <- list(status_code = 503)
-    expect_error(response_check(response))
-})
+test_that(desc = "Error is raised if 503 received from server.",
+          code = {
+            response <- list(status_code = 503)
+            expect_error(response_check(response))
+          })
+
+test_that(desc = "Error is raised when response type is not application/json.",
+          code = {
+            response <- structure(list(
+              url = "foo.bar",
+              status_code = 200,
+              headers = list(`Content-Type` = "text/html")
+            ),
+            class = "response")
+            expect_error(response_check(response),
+                         "Prometheus API did not return json")
+          })
+
+test_that(desc = "OK when application type is json and status code is 200.",
+          code = {
+            response <- structure(list(
+              url = "foo.bar",
+              status_code = 200,
+              headers = list(`Content-Type` = "application/json")
+            ),
+            class = "response")
+            expect_silent(response_check(response))
+          })
 
 # Test whether package can pull some sample data, only run if connection
 # available and host is responsive
