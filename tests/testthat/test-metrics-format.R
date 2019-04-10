7e0294ededf0538d9f6fed23f262f2c8561b2fc1
Konrad Zdeb <konrad.zdeb@me.com>
2019-04-10 17:09:14 +0100

    Metrics raw
    Added structure for response like object representing raw metrics to be created
    
    Signed-off-by: Konrad Zdeb <konrad.zdeb@me.com>

--- tests/testthat/test-metrics-format.R
+++ tests/testthat/test-metrics-format.R
@@ -38,4 +38,37 @@ test_that(desc = "Range metrics are not empty",
 # }
 
 # Create response-like objects
-
+# Metrics raw
+metricsRaw <- list(status = "success",
+                   data = list(
+                     resultType = "vector",
+                     result = structure(
+                       list(
+                         metric = structure(
+                           list(
+                             `__name__` = c("up", "up", "up",
+                                            "up"),
+                             instance = c(
+                               "demo.robustperception.io:9090",
+                               "demo.robustperception.io:9091",
+                               "demo.robustperception.io:9093",
+                               "demo.robustperception.io:9100"
+                             ),
+                             job = c("prometheus", "pushgateway", "alertmanager", "node")
+                           ),
+                           class = "data.frame",
+                           row.names = c(NA, 4L)
+                         ),
+                         value = list(
+                           c("1554912322.161", "1"),
+                           c("1554912322.161", "1"),
+                           c("1554912322.161",
+                             "1"),
+                           c("1554912322.161", "1")
+                         )
+                       ),
+                       class = "data.frame",
+                       row.names = c(NA,
+                                     4L)
+                     )
+                   ))
