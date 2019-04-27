6f3e25cfd02380634191bb6c73b13dd03a4b899a
Glenn McDonald <2371316+glenn-m@users.noreply.github.com>
2019-04-28 00:14:25 +0100

    Fixing curl WARNINGS

--- tests/testthat/test-response-check.R
+++ tests/testthat/test-response-check.R
@@ -70,25 +70,23 @@ test_that(desc = "OK when application type is json and status code is 200.",
             expect_silent(response_check(response))
           })
 
-# Test whether package can pull some sample data, only run if connection
-# available and host is responsive
-if (requireNamespace(package = "curl", quietly = TRUE)) {
-    # Source query data once to use later across tests
-    prom <-
-      Prometheus$new(host = "http://demo.robustperception.io", port = 9090)
-    dta_tst_metrics <-
-      prom$query(query = "go_goroutines",
-                 time = as.numeric(as.POSIXct(Sys.time())))
+# Test whether package can pull some sample data
+# Source query data once to use later across tests
+prom <-
+  Prometheus$new(host = "http://demo.robustperception.io", port = 9090)
+dta_tst_metrics <-
+  prom$query(query = "go_goroutines",
+             time = as.numeric(as.POSIXct(Sys.time())))
 
-    test_that(desc = "Metrics within Prometheus class are data frame.",
-              code = expect_is(object = dta_tst_metrics,
-                               class = "data.frame"))
+test_that(desc = "Metrics within Prometheus class are data frame.",
+          code = expect_is(object = dta_tst_metrics,
+                           class = "data.frame"))
 
-    test_that(desc = "Metrics data frame has some columns.",
-              code = expect_gt(object = ncol(dta_tst_metrics),
-                               expected = 2))
+test_that(desc = "Metrics data frame has some columns.",
+          code = expect_gt(object = ncol(dta_tst_metrics),
+                           expected = 2))
 
-    test_that(desc = "Metrics data frame has some rows.",
-              code = expect_gt(object = nrow(dta_tst_metrics),
-                               expected = 2))
-}
+test_that(desc = "Metrics data frame has some rows.",
+          code = expect_gt(object = nrow(dta_tst_metrics),
+                           expected = 2))
+
