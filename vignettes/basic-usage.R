24729c62a2666af1b0c426a5d77c62b5ddc085ab
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-11 12:56:39 +0100

    Vignettes
    Everything works as it should - needed to refersh the build.

--- vignettes/basic-usage.R
+++ vignettes/basic-usage.R
@@ -1,6 +1,41 @@
 ## ----setup, include = FALSE----------------------------------------------
 knitr::opts_chunk$set(
+  echo = TRUE,
   collapse = TRUE,
   comment = "#>"
 )
+
+# Enable httptest for vignette development
+library(httptest)
+start_vignette("basic-usage")
+
+# Load package
+library("promR")
+
+## ----example_class-------------------------------------------------------
+# library("promR")
+prom <- Prometheus$new(host = "http://demo.robustperception.io", port = 9090)
+
+## ----example_instant-----------------------------------------------------
+# Timestamps can be in rfc3339 or unix format
+metrics_instant <- prom$query(query = "go_goroutines", 
+                              time = as.POSIXct("2019-08-11 12:25:57 BST"))
+
+## ----show_instant_results, echo=FALSE------------------------------------
+knitr::kable(metrics_instant)
+
+## ----example_range-------------------------------------------------------
+metrics_range <- prom$rangeQuery(
+  query = "go_goroutines",
+  start = as.numeric(as.POSIXct("2019-08-11 12:20:32 BST")),
+  end = as.numeric(as.POSIXct("2019-08-11 12:30:46 BST")),
+  step = "10s"
+)
+
+## ----show_range_results, echo=FALSE--------------------------------------
+head(metrics_range, n = 1)
+
+## ----end-setup, include=FALSE--------------------------------------------
+# Close httptes vignette support
+end_vignette()
 
