40609c67c3670efaa1a668c76b3186c74c4190ca
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-16 16:06:59 +0100

    Travis
    Fixed broken test.

--- tests/testthat/test-api-responses-instant-query.R
+++ tests/testthat/test-api-responses-instant-query.R
@@ -4,9 +4,10 @@ with_mock_api({
   test_that(desc = "promR instant query gets response",
             code = {
               metrics_instant <- prom$query(query = "go_goroutines",
-                                            time = as.POSIXct("2019-05-09 21:11:41 BST"))
+                                            time = as.numeric(
+                                              as.POSIXct("2019-08-09 21:11:41 BST")))
               expect_is(object = metrics_instant, class = "data.frame")
-              expect_gt(object = ncol(metrics_instant),expected = 2)
+              expect_gt(object = ncol(metrics_instant), expected = 2)
               expect_gt(object = nrow(metrics_instant), expected = 2)
               expect_named(object = metrics_instant)
             })
