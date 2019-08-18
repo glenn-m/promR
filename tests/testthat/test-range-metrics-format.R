47271ec6f7cdd46fabbab854656d3a52537a9950
Konrad Zdeb <konrad.zdeb@me.com>
2019-08-18 12:57:54 +0100

    Testing
    Moved some tests to with_mock_api as they use object derived via query, this should be now handled by httptest.

--- tests/testthat/test-range-metrics-format.R
+++ tests/testthat/test-range-metrics-format.R
@@ -1,19 +1,36 @@
 context("Range metrics data is formatted correctly")
 
-test_that(desc = "Range metrics results are data frame",
-          code = expect_is(object = metrics_range,
-                           class = "data.frame"))
+with_mock_api({
+  prom <- Prometheus$new(host = "http://demo.robustperception.io", port = 9090)
+  metrics_range <- prom$rangeQuery(
+    query = "go_goroutines",
+    start = as.numeric(as.POSIXct("2019-08-11 12:20:32 BST")),
+    end = as.numeric(as.POSIXct("2019-08-11 12:30:46 BST")),
+    step = "10s"
+  )
+  metrics_instant <- prom$query(query = "go_goroutines",
+                                time = as.numeric(as.POSIXct("2019-08-11 12:25:57 BST")))
 
-test_that(desc = "Range metrics are named",
-          code = expect_named(object = metrics_range))
+  test_that(desc = "Range metrics results are data frame",
+            code = expect_is(object = metrics_range,
+                             class = "data.frame"))
 
-test_that(desc = "Instant metrics are not empty",
-          code = expect_gte(object = nrow(metrics_current),
-                            expected = 2))
+  test_that(desc = "Range metrics are named",
+            code = expect_named(object = metrics_range))
 
-test_that(desc = "Error is generated for non-data-frame",
-          code = expect_error(object = format_metrics_range_data(x = 1),
-                              regexp = "^Assertion on \\'x\\'\\sfailed:"))
+  test_that(desc = "Instant metrics are not empty",
+            code = expect_gte(object = nrow(metrics_instant),
+                              expected = 2))
+
+})
+
+
+
+test_that(
+  desc = "Error is generated for non-data-frame",
+  code = expect_error(object = format_metrics_range_data(x = 1),
+                      regexp = "^Assertion on \\'x\\'\\sfailed:")
+)
 
 # Source, response-like objects
 test_that(desc = "Column __name__ in data frame is renamed to name",
