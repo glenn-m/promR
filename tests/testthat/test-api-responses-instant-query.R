5d483cadb9d921ce6fe1fe3683b6080d99531443
Konrad Zdeb <konrad.zdeb@me.com>
2019-09-07 20:44:14 +0100

    Referncing the same timestamps as newly created files.

--- tests/testthat/test-api-responses-instant-query.R
+++ tests/testthat/test-api-responses-instant-query.R
@@ -4,8 +4,7 @@ with_mock_api({
   test_that(desc = "promR instant query gets response",
             code = {
               metrics_instant <- prom$query(query = "go_goroutines",
-                                            time = as.numeric(
-                                              as.POSIXct("2019-08-09 21:11:41 BST")))
+                                            time = as.numeric(as.POSIXct("2019-09-05 12:25:57 BST")))
               expect_is(object = metrics_instant, class = "data.frame")
               expect_gt(object = ncol(metrics_instant), expected = 2)
               expect_gt(object = nrow(metrics_instant), expected = 2)
